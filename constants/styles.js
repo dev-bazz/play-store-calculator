dbab813885d955fa23148172149b8d3c3c29c388
bazz <clement.f.bazz@gmail.com>
2023-06-18 16:17:14 +0100

    feat: :seedling: base files

---
+++ constants/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "./colorsUpdate";
+
+export const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: COLORS.neutral.black_b,
+	},
+	text_global: {
+		color: COLORS.neutral.white,
+	},
+});
