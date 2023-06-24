f7f1f9a5e27483718af8ed4f5aecc19687071beb
bazz <clement.f.bazz@gmail.com>
2023-06-24 14:27:20 +0100

    feat: :seedling: Added Prompt Font

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { AppState, Theme, useAppState, useCalculator } from "../hook";
 import Calculator from "./Calculator";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function index() {
 	return (
